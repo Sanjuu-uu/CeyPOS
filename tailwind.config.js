24a0444995ad7b161a58f4d5beacfaf7701266a0
Sanjula Deshapriya <deshapriya.sanjula@gmail.com>
2025-05-29 20:17:29 +0530

    update your tailwind configuration and defined nessasary dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './index.html', 
+    './src/**/*.{js,ts,jsx,tsx}',
+    './component-library/src/**/*.{js,ts,jsx,tsx}',
+    './system-administration-monitoring/src/**/*.{js,ts,jsx,tsx}',
+    './webmobile/**/*.{js,ts,jsx,tsx}'
+  ],
   theme: {
     extend: {
       fontFamily: {
