54ce961df5a74a0be4dea08f6f23fda6d84cc5dc
Tekshan2006 <tekshan.thaksara@gmail.com>
2025-09-27 18:16:06 +0530

    update the validating origin

--- server/index.js
+++ server/index.js
@@ -8,7 +8,12 @@ const PORT = process.env.PORT || 8080;
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+    origin: [
+      "http://localhost:5173",
+      "http://127.0.0.1:5173",
+      "https://ceypossolutions.com",
+      "https://www.ceypossolutions.com",
+    ],
     credentials: true,
   })
 );
